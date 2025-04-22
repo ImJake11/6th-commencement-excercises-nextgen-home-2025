c2a98924cebe5492c3026e2047b2e307bb05eec8
ImJake11 <jakejuguilon843@gmail.com>
2025-04-22 14:27:47 +0800

    second try

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import {  useState } from 'react'
 import './App.css'
 import "./styles/styles.css";
-import img1 from "./assets/1.png";
-import img2 from "./assets/2.png";
-import img3 from "./assets/3.png";    
-import img4 from "./assets/4.png";
-import img5 from "./assets/5.png";
-import img6 from "./assets/6.png";
-import img7 from "./assets/7.png";   
-import img8 from "./assets/8.png";
-import img9 from "./assets/9.png";
-import img10 from "./assets/10.png";    
-import img11 from "./assets/11.png";
-import img12 from "./assets/12.png";
-import img13 from "./assets/13.png";
-import img14 from "./assets/14.png";   
-import img15 from "./assets/15.png";
-import img16 from "./assets/16.png";
-import img17 from "./assets/17.png";    
-import img18 from "./assets/18.png";
-import img19 from "./assets/19.png";
-import img20 from "./assets/20.png";
-import img21 from "./assets/21.png";
-import img22 from "./assets/22.png";   
+import img1 from "./assets/1.webp";
+import img2 from "./assets/2.webp";
+import img3 from "./assets/3.webp";    
+import img4 from "./assets/4.webp";
+import img5 from "./assets/5.webp";
+import img6 from "./assets/6.webp";
+import img7 from "./assets/7.webp";   
+import img8 from "./assets/8.webp";
+import img9 from "./assets/9.webp";
+import img10 from "./assets/10.webp";    
+import img11 from "./assets/11.webp";
+import img12 from "./assets/12.webp";
+import img13 from "./assets/13.webp";
+import img14 from "./assets/14.webp";   
+import img15 from "./assets/15.webp";
+import img16 from "./assets/16.webp";
+import img17 from "./assets/17.webp";    
+import img18 from "./assets/18.webp";
+import img19 from "./assets/19.webp";
+import img20 from "./assets/20.webp";
+import img21 from "./assets/21.webp";
+import img22 from "./assets/22.webp";   
 import logo from "./assets/logo.png";
 
 
