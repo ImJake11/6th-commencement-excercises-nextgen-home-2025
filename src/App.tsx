3aaa21acb025e268945e18ec7b322d856592c62c
ImJake11 <jakejuguilon843@gmail.com>
2025-04-22 14:00:07 +0800

    ist protype

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,52 @@ import img2 from "./assets/2.png";
 import img3 from "./assets/3.png";    
 import img4 from "./assets/4.png";
 import img5 from "./assets/5.png";
+import img6 from "./assets/6.png";
+import img7 from "./assets/7.png";   
+import img8 from "./assets/8.png";
+import img9 from "./assets/9.png";
+import img10 from "./assets/10.png";    
+import img11 from "./assets/11.png";
+import img12 from "./assets/12.png";
+import img13 from "./assets/13.png";
+import img14 from "./assets/14.png";   
+import img15 from "./assets/15.png";
+import img16 from "./assets/16.png";
+import img17 from "./assets/17.png";    
+import img18 from "./assets/18.png";
+import img19 from "./assets/19.png";
+import img20 from "./assets/20.png";
+import img21 from "./assets/21.png";
+import img22 from "./assets/22.png";   
+import logo from "./assets/logo.png";
+
+
 
 function App() {
 
   const imgs = [
     img1,
-   img2,
+    img2,
     img3,
     img4,
     img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
   ];
   const [flippedPages, setFlippedPages] = useState<number[]>([]);
 
@@ -33,10 +70,34 @@ function App() {
 
 const handlePrevious = () => {  
     setFlippedPages((prev) => prev.slice(0,-1));
+
+
+}
+
+const handleReset = () => {
+setFlippedPages([]);
 }
 
     return (
-        <div className="pad">
+       <div style={{
+        width: "100vw",
+        height: "100vh",
+        position:"relative",
+        display:"flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+        backgroundColor: "#eeeeee",
+        margin: "0",
+        gap: "40px",
+       }}>
+
+        {/** LOGO */}
+        <img src={logo} alt="logo" width={40} />
+
+
+         <div className="pad">
             <div className="book">
                 {imgs.map((img, index) => (
                     <div
@@ -45,17 +106,20 @@ const handlePrevious = () => {
                         style={{ zIndex: imgs.length - index }} // Ensure pages stack properly
                     >
                         <div className="page-content"><img src={img} alt="image"/></div>
-                        <div className="page-shadow"></div>
+           
                     </div>
                 ))}
             </div>
-            {/** BUTTONS */}
-
-            <div className="controls">
-              <button className="prev-button" onClick={handlePrevious}>Previous</button>
-              <button className='next-button' onClick={handleNext}>Next</button>
-            </div>
+          
         </div>
+          {/** BUTTONS */}
+
+          <div className="controls">
+              <button className="prev-button" onClick={handlePrevious} disabled={flippedPages.length === 0}>Previous</button>
+              <button className='reset-button' onClick={handleReset} disabled={flippedPages.length === 0}>Reset</button>
+              <button className='next-button' onClick={handleNext} disabled={flippedPages.length === ( imgs.length - 1 )}>Next</button>
+            </div>
+       </div>
     );
 }
 
